2bd31b41387274aad5e681ad8b1ff40a5cfd07cc
tony woode <tonywoodecode@gmail.com>
2019-03-03 22:13:22 +0000

    configure testing deps

---
+++ specs/init.js
@@ -0,0 +1,9 @@
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+const sinonChai = require("sinon-chai")
+
+chai.use(chaiAsPromised)
+chai.use(sinonChai)
+
+global.sinon = require('sinon')
+global.expect = chai.expect
