f314d1ddd2c67b44881f138cf637144a3f56ff79
tony woode <tonywoodecode@gmail.com>
2019-03-03 21:25:36 +0000

    swap async/await for FP Task, gain error track

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 const config = require("../config.json")
 const client = require("./client")
-// Get directory contents
-const nasDirListing = async => client.getDirectoryContents(config.folder)
+const Task = require("data.task")
 
-const printNasDirListing = async () => console.log(await nasDirListing())
+//promise to task
+const nasDirListing = new Task((rej, res) =>
+  client
+    .getDirectoryContents(config.folder)
+    .then(res)
+    .catch(rej)
+)
 
-printNasDirListing()
-// Outputs a structure like:
-// [{
-//     filename: "/my-file.txt",
-//     basename: "my-file.txt",
-//     lastmod: "Mon, 10 Oct 2018 23:24:11 GMT",
-//     size: 371,
-//     type: "file"
-// }]
+nasDirListing.fork(console.log, console.log)
