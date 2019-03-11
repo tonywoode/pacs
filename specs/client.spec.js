7db88a62f1c04ddb68f87716d7e4ade434312f1c
tony woode <tonywoodecode@gmail.com>
2019-03-11 14:16:28 +0000

    first test passing

--- specs/client.spec.js
+++ specs/client.spec.js
@@ -8,19 +8,33 @@ const config = { url: "http://localhost", port: 1900 }
 //start webdav server serving a fake filesystem, you'll get 200s back
 const server = require(`${src}server`)(webdavServer)(config)
 const client = require(`${src}client`)(webdavClient)(config)
-//console.log(server)
-client.stat("folder1/file1.txt").then(result => {
-  console.log( "result is " + JSON.stringify(result.filename))
-  //process.exit()
-  server.stop()
-  })
-    .catch(console.log)
 
-//describe(`client`, () => {
-//  describe(`stat`, () => {
-//    it(`should lookup a file`, () => {})
-    //so we have a server running on port 1900 let's run a stat command to get a listing eh
+const clientTaskified = require(`${src}taskifyPromiseModule`)(client)
+//client.stat("folder1/file1.txt").then(result => {
+//  console.log( "result is " + JSON.stringify(result.filename))
+//  server.stop()
 //  })
-//})
+//    .catch(console.log)
+const newError = msg => {
+  throw new Error(msg)
+}
 
-//server.stop() // server.stop() is not working for me, and nothing else depends on this test
+describe(`client`, () => {
+  afterEach(() => server.stop())
+  const file = "/folder1/file1.txt"
+  describe(`stat`, () => {
+    it(`should lookup a file`, done => {
+      clientTaskified.stat(file).fork(
+        // don't even think about writing: `rej =>`
+        done //done(`don't even think about invoking done`))
+        , res => { 
+          expect(res.filename).to.equal(file)          
+          done()
+        }
+        )
+
+    })
+  //so we have a server running on port 1900 let's run a stat command to get a listing eh
+  })
+})
+
