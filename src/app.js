0c653c7c173ad0bec5036f3d56d45c6fecdde903
tony woode <tonywoodecode@gmail.com>
2019-04-01 19:39:58 +0100

    test on windows, performance is good

--- src/app.js
+++ src/app.js
@@ -3,6 +3,23 @@ const webdav = require('webdav-server').v2
 const app = require('express')()
 const server = new webdav.WebDAVServer()
 const testDir = './specs/testDir'
-server.setFileSystem('', new webdav.PhysicalFileSystem(testDir), console.log("ready"))
+const testWinDir = "F:/Computer Classics/c64 Games"
+
+
+server.setFileSystem('', new webdav.PhysicalFileSystem(testWinDir), console.log("ready"))
 app.use(webdav.extensions.express('', server))
+
+
+server.afterRequest((arg, next) => {
+    // Display the method, the URI, the returned status code and the returned message
+    console.log('>>', arg.request.method, arg.requested.uri, '>', arg.response.statusCode, arg.response.statusMessage);
+    console.log(arg.request.url)
+    console.log( arg.request.rawHeaders)
+    console.log(arg.request.headers )
+
+    // If available, display the body of the response
+    console.log(arg.responseBody);
+    next();
+});
+
 app.listen(1900)
