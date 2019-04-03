2ea07881be742ee988fbf23fa073baf4609382e7
tony woode <tonywoodecode@gmail.com>
2019-04-03 18:03:03 +0100

    print req, send custom message

--- src/app.js
+++ src/app.js
@@ -3,23 +3,35 @@ const webdav = require('webdav-server').v2
 const app = require('express')()
 const server = new webdav.WebDAVServer()
 const testDir = './specs/testDir'
-const testWinDir = "F:/Computer Classics/c64 Games"
+//const testWinDir = "F:/Computer Classics/c64 Games"
+const testWinDir = "F:/Nintendo Games/N64 Games/GoodN64_314_GM"
 
 
-server.setFileSystem('', new webdav.PhysicalFileSystem(testWinDir), console.log("ready"))
+app.all('/', (req,res) => {
+  console.log(req)
+  res.send("hello there")
+
+})
+// or just
+app.propfind('/', (req,res) => {
+  console.log(req)
+  res.send("hello there")
+
+})
+server.setFileSystem('', new webdav.PhysicalFileSystem(testDir), console.log("ready"))
 app.use(webdav.extensions.express('', server))
 
 
-server.afterRequest((arg, next) => {
-    // Display the method, the URI, the returned status code and the returned message
-    console.log('>>', arg.request.method, arg.requested.uri, '>', arg.response.statusCode, arg.response.statusMessage);
-    console.log(arg.request.url)
-    console.log( arg.request.rawHeaders)
-    console.log(arg.request.headers )
-
-    // If available, display the body of the response
-    console.log(arg.responseBody);
-    next();
-});
+//server.afterRequest((arg, next) => {
+//    // Display the method, the URI, the returned status code and the returned message
+//    console.log('>>', arg.request.method, arg.requested.uri, '>', arg.response.statusCode, arg.response.statusMessage);
+//    console.log(arg.request.url)
+//    console.log( arg.request.rawHeaders)
+//    console.log(arg.request.headers )
+//
+//    // If available, display the body of the response
+//    console.log(arg.responseBody);
+//    next();
+//});
 
 app.listen(1900)
