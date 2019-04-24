7e11fe136f6f3bde4ca446227a2b7b9259e79053
tony woode <tonywoodecode@gmail.com>
2019-04-24 20:36:41 +0100

    fix non-persistent tcp connections...
    ...node's http module is certainly not alone in this, look
    at the webdav clients on operating systems...

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const webdav = require('webdav-server').v2
 const app = require('express')()
 const request = require('request');
 const basicAuth = require('express-basic-auth');
+const proxy = require('http-proxy-middleware')
 
 const server = new webdav.WebDAVServer()
 const config = require("../config.json")
@@ -41,21 +42,40 @@ else {
   // option1: https://stackoverflow.com/a/16924410/3536094
   //or rather: https://stackoverflow.com/a/20539239/3536094
   //error handling if the remote server is offline will need fixing though: though http://stackoverflow.com/a/20198377/132208
- 
+  
+//after a while coping with connection reuse issues piping request, this option reuses connections correctly, at least with netdrive....
+//https://stackoverflow.com/questions/10435407/proxy-with-express-js/16924410
 
-  app.all('*', (req,res) => {
-      req.headers.Authorization = headerAuth
-    //     console.log(req.headers)
-    //   console.log(req.body)
-    //   console.log(req.method)
-    //   console.log(req.params)
-    //console.log("PATH" + req.path)
-      //modify the url in any way you want
-    var newurl = `${config.localIp}:${config.port}${req.path}`
-    //  console.log("newurl is " + newurl)
-      req.pipe(request(newurl)).pipe(res)
-      //  console.log(res)
-  })
+    // we must enable persistent connections in node, as underlying this all is the 
+    // http lib's default of http 1.0-like new connections for each request
+    // https://stackoverflow.com/a/38614839/3536094
+    const http = require('http')
+    const keepAliveAgent = new http.Agent({ keepAlive: true });
+
+    var myProxy = proxy('/', {
+      auth : `${config.user}:${config.pass}`,
+      target: `${config.remoteIp}:${config.port}`,
+      agent: keepAliveAgent
+    })
+app.use(myProxy)
+
+  //  app.all('*', (req,res) => {
+  //    //req.headers.Authorization = headerAuth
+  //    req.headers.connection = "keep-alive"
+  //    //console.log(req.headers.Authorization)
+  //      console.log(req.headers)
+  //       console.log(req.body)
+  //       console.log(req.method)
+  //       console.log(req.params)
+  //    //console.log("PATH" + req.path)
+  //      //modify the url in any way you want
+  //    //    var newurl = `${config.localIp}:${config.port}${req.path}`
+  //    //  console.log("newurl is " + newurl)
+  //    //  req.pipe(request(newurl)).pipe(res)
+  //    console.log(res.headers)
+  //    console.log(res.method)
+  //    console.log(res.params)
+  //  })
 
 }
 
