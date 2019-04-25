ef199e6bcad6b25231b5e41e0e93634de435d269
tony woode <tonywoodecode@gmail.com>
2019-04-25 20:55:09 +0100

    add keepAlive agent to client

--- src/client.js
+++ src/client.js
@@ -1,9 +1,12 @@
 'use strict'
+const http = require('http')
+const keepAliveAgent = new http.Agent({ keepAlive: true });
 const client = webdavClient => config => webdavClient.createClient(
     `${config.localIp}:${config.port}`,
     {
         username: config.user,
-        password: config.pass
+      password: config.pass,
+      httpAgent: keepAliveAgent //don't forget we'll need an https keep alive agent too
     }
 )
 
