e136def97df869a362a3db4aa587d66539505475
tony woode <tonywoodecode@gmail.com>
2019-05-12 22:37:50 +0100

    aim is to send these on request for them

---
+++ src/annoyingFiles.js
@@ -0,0 +1,24 @@
+PROPFIND /Games/desktop.ini
+
+proxyRes header is {
+    "date": "Sun, 12 May 2019 19:27:12 GMT",
+      "server": "Apache",
+      "content-length": "215",
+      "keep-alive": "timeout=5, max=100",
+      "connection": "Keep-Alive",
+      "content-type": "text/html; charset=iso-8859-1"
+}
+body is
+  <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+  <html><head>
+  <title>404 Not Found</title>
+  </head><body>
+  <h1>Not Found</h1>
+  <p>The requested URL /Games/desktop.ini was not found on this server.</p>
+  </body></html>
+
+
+
+
+
+
